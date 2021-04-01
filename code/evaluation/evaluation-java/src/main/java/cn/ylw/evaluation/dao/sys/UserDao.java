bf427bc89bfb668fd7a1f29eb13b74afb6045a5c
linwen <1262700225@qq.com>
2021-04-01 21:28:37 +0800

    前后端分离 - 后端项目初建

--- code/evaluation/evaluation-java/src/main/java/cn/ylw/evaluation/dao/sys/UserDao.java
+++ code/evaluation/evaluation-java/src/main/java/cn/ylw/evaluation/dao/sys/UserDao.java
@@ -1,9 +1,9 @@
-package cn.ylw.evaluation.mapper.sys;
+package cn.ylw.evaluation.dao.sys;
 
 /**
  * @author: ylw
  * @date: 2021年04月01日 19时26分
  * @description:
  */
-public interface UserMapper {
+public interface UserDao {
 }
