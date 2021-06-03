f6eefdea9a76f81928391b64bb076dec727888c0
linwen <1262700225@qq.com>
2021-06-03 15:00:40 +0800

    javaScript

--- code/evaluation/evaluation-java/service/src/main/java/cn/ylw/evaluation/service/sys/impl/AccountServiceImpl.java
+++ code/evaluation/evaluation-java/service/src/main/java/cn/ylw/evaluation/service/sys/impl/AccountServiceImpl.java
@@ -7,6 +7,7 @@ import cn.ylw.evaluation.dao.sys.AccountDao;
 import cn.ylw.evaluation.entity.sys.Account;
 import cn.ylw.evaluation.enums.StatusEnum;
 import cn.ylw.evaluation.service.sys.AccountService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
@@ -23,7 +24,7 @@ import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
 @Service
 @Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class AccountServiceImpl implements AccountService {
+public class AccountServiceImpl extends ServiceImpl implements AccountService {
     private final AccountDao accountDao;
 
     @Override
